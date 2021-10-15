f3eb075ba8889f134800f32ff303f73f5d5f3e82
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-10-15 22:53:55 +1100

    Adds more icons, updates mannifest and sw.js to make pwa installable

--- src/sw.js
+++ src/sw.js
@@ -3,15 +3,17 @@ var urlsToCache = [
   ".",
   "index.html",
   "styles.css",
-  "majifest.json",
+  "manifest.json",
   "index.js",
   "favicon.ico",
   "confetti.js",
 ];
 self.addEventListener("install", function (event) {
+  console.log("here");
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
+self.addEventListener("fetch", () => console.log("fetch"));
